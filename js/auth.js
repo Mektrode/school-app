0c25290b457c6ebdd6948e0580e833db95d8ff91
M-Hc0d3 <mowz971@gmail.com>
2013-06-06 20:40:20 +0100

    working on auth system
    starting on new pages too

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,36 @@
-var user = form.;
-var pass = form.;
 
 
+function login(){
+	var user = document.login.userID.value;
+	var pass = document.login.pass.value;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://my php server here", user, pass, false);
+	
+	// Add your code below!
+	xhr.send();
+
+	    this.http.open("get", this.action, false, username, password);
+	    this.http.send("");
+	    if (http.status == 200) {
+	        document.location = this.action;
+	    } else {
+	        alert("Incorrect username and/or password.");
+	    }
+
+
+	if () {
+		//load home page
+
+	}
+	else if (xhr.status === 0){
+		//error logging in!
+		errorPass();
+	}
+
+	console.log (xhr.status);
+	console.log (xhr.statusText);
+}
 
 
 // Http request post
@@ -10,13 +39,6 @@ var pass = form.;
 
 // if error happens, ask user to retype 
 function errorPass (){
-
-}
-
-if (auth){
-	// In another js file!
-	getJSON();
-}
-else {
-	errorPass();
+	//alert popup box!
+	alert("Wrong UserID or Password!");
 }
